2858f5e02fedde60d81dbcfb11b50901c3fa5a68
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2014-04-13 20:20:45 +0200

    R Programming: completed PA2

--- r_programming/ProgrammingAssignment2/cachematrix.R
+++ r_programming/ProgrammingAssignment2/cachematrix.R
@@ -1,15 +1,58 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    chachedSolve <- NULL
+    set <- function(y) {
+        x <<- y
+        chachedSolve <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) chachedSolve <<- solve
+    getsolve <- function() chachedSolve
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    chachedSolve <- x$getsolve()
+    if(!is.null(chachedSolve)) {
+        message("getting cached data")
+        return(chachedSolve)
+    }
+    data <- x$get()
+    solve <- solve(data, ...)
+    x$setsolve(solve)
+    solve
+}
+
+
+
+## This function is a simple unittest for makeCacheMatrix and cacheSolve
+
+unittest <- function() {
+    # prepare data
+    matrix = matrix(runif(25), c(5, 5))
+    correctSolution = solve(matrix)
+    cacheMatrix = makeCacheMatrix(matrix)
+    # cache should be empty
+    stopifnot(is.null(cacheMatrix$getsolve()))
+    # calculate solve
+    solution <- cacheSolve(cacheMatrix)
+    stopifnot(identical(solution, correctSolution))
+    # cache should be filled
+    stopifnot(identical(cacheMatrix$getsolve(), correctSolution))
+    # set different matrix
+    cacheMatrix$set(matrix(0))
+    # cache should be empty again
+    stopifnot(is.null(cacheMatrix$getsolve()))
 }
