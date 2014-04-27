3b7c9a7ea5380eeab8cfbe59d388025ac2bd1bf8
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2014-04-27 10:36:13 +0200

    Getting and Cleaning Data: get_data cleanup

--- cleaning_data/get_data.R
+++ cleaning_data/get_data.R
@@ -1,13 +1,14 @@
 
 # This script downloads and unzips SAMSUNG UCI HAR Dataset
 # into current working directory.
-# It uses method = "curl", so it might not work
-# if you don't have curl installed.
+# It uses method = "curl", so if you don't have curl installed
+# you might want to use different method.
 
-download_data <- function() {
+get_data <- function(method = "curl") {
     message("Downloading UCI HAR Dataset")
     download.file(
         "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-        "dataset.zip", "curl")
+        "dataset.zip",
+        method)
     unzip("dataset.zip")
 }
