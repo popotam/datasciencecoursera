ce7019389f6acfe4232d58f20c227d9f612b9e48
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2014-05-06 20:27:08 +0200

    Plot 1 completed - it's perfect

--- plot1.R
+++ plot1.R
@@ -1,3 +1,17 @@
 plot1 <- function(data) {
-    
+    png(
+        "plot1.png",
+        width = 504,
+        height = 504,
+        units = "px",
+        pointsize = 12,
+        bg = "transparent"
+    )
+    hist(
+        data$Global_active_power,
+        col = "red",
+        main = "Global Active Power",
+        xlab = "Global Active Power (kilowatts)"
+    )
+    dev.off()
 }
