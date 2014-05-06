2ec1f1ab0c07df08365838aa4e3d6318286e84bd
Paweł Sobkowiak <pawel.sobkowiak@gmail.com>
2014-05-06 20:38:45 +0200

    Plot 2 completed - it's perfect

--- plot2.R
+++ plot2.R
@@ -1,3 +1,25 @@
 plot2 <- function(data) {
-    
+    # set locale to en_US to get English names of day
+    Sys.setlocale("LC_ALL", "en_US")
+    # proceed with the plotting
+    png(
+        "plot2.png",
+        width = 504,
+        height = 504,
+        units = "px",
+        pointsize = 12,
+        bg = "transparent"
+    )
+    plot(
+        data$Time,
+        data$Global_active_power,
+        type="n",
+        xlab = "",
+        ylab = "Global Active Power (kilowatts)"
+    )
+    lines(
+        data$Time,
+        data$Global_active_power
+    )
+    dev.off()
 }
